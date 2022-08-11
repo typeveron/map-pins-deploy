b01c1eb62d928266027634456678098fb11801e9
Bechir Mihoub <bech@Bechirs-iMac.local>
2022-08-11 02:18:46 -0400

    updated dependancies

--- frontend/src/components/MapPage.js
+++ frontend/src/components/MapPage.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, Fragment} from 'react';
 import Map, {Marker, Popup} from 'react-map-gl';
-import { Room, Star } from "@material-ui/icons";
+import { Room, Star } from "@mui/icons-material";
 import axios from 'axios';
 import {format} from 'timeago.js';
 import "./mappage.css";
