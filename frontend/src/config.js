86a6b7b2a921a2897fc0adfc8bc7bf959e8e221c
Bechir Mihoub <bech@Bechirs-iMac.local>
2022-08-12 16:09:19 -0400

    fixing the base url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://map-add-pins.herokuapp.com/api"
+    baseURL: "https://map-add-pins.herokuapp.com/api/"
 })
