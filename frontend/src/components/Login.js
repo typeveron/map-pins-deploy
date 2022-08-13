54093e49088e84d0ba7ab9b75e49c481348d69ca
Bechir Mihoub <bech@Bechirs-iMac.local>
2022-08-12 23:33:01 -0400

    trying to fix route issue

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -25,7 +25,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-          const {data} = await axios.post(process.env.REACT_APP_LOGIN, {
+          const {data} = await axios.post("https://add-pins-to-map.herokuapp.com/api/signin", {
             username, password
           });
           console.log(data);
