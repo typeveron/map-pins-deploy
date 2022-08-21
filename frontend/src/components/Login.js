6535c8d98b632ce4dcdf6009bcb0e372deee2d23
Bechir Mihoub <bech@Bechirs-iMac.local>
2022-08-21 00:30:19 -0400

    trying

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -38,8 +38,8 @@ export default function Login() {
             navigate("/map");
           }
       } catch (err) {
-        toast.error(`There is an error ${err.response}`);
-        console.log(`This is the error: ${err.response}`);
+        toast.error(`There is an error ${err.response.data}`);
+        console.log(`This is the error: ${err.response.data}`);
       }
     } 
 
